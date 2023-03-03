3918203c38b9d0a6b31fa97041ccfd9e834ec2bc
Juan Sebastián Cañas <jscanass@gmail.com>
2023-03-03 16:23:12 +0000

    Update train using yaml files

--- baseline/train.py
+++ baseline/train.py
@@ -19,31 +19,23 @@ from time import time
 
 from util import init_seed, min_max_normalize
 
-DIR_VSCODE = "/mnt/batch/tasks/shared/LS_root/mounts/clusters/gpu-baseline/code/Users/jscanass/anuraset/baseline"
-os.chdir(DIR_VSCODE)
-
-#ANNOTATIONS_FILE = 'metadata_sample1000.csv' 
-ANNOTATIONS_FILE = "anuraset_pruebas/data/datasets/anurasetv3/metadata.csv"
-AUDIO_DIR =  "anuraset_pruebas/data/datasets/anurasetv3/audio/"
-SAMPLE_RATE = 22050 
-NUM_SAMPLES = SAMPLE_RATE*3
-
-def load_model(cfg,folder_name):
+def load_model(cfg):
     '''
         Creates a model instance and loads the latest model state weights.
     '''
     model_instance = ResNetClassifier(model_type=cfg['model_type'])        # create an object instance of our CustomResNet18 class
+    folder_name = cfg['folder_name']
 
     # load latest model state
-    model_states = glob.glob(f'model_states/{folder_name}/*.pt')
+    model_states = glob.glob(f'baseline/model_states/{folder_name}/*.pt')
     if len(model_states):
         # at least one save state found; get latest
-        model_epochs = [int(m.replace(f'model_states/{folder_name}','').replace('.pt','')) for m in model_states]
+        model_epochs = [int(m.replace(f'baseline/model_states/{folder_name}','').replace('.pt','')) for m in model_states]
         start_epoch = max(model_epochs)
 
         # load state dict and apply weights to model
         print(f'Resuming from epoch {start_epoch}')
-        state = torch.load(open(f'model_states/{folder_name}{start_epoch}.pt', 'rb'), map_location='cpu')
+        state = torch.load(open(f'baseline/model_states/{folder_name}{start_epoch}.pt', 'rb'), map_location='cpu')
         model_instance.load_state_dict(state['model'])
 
     else:
@@ -53,27 +45,26 @@ def load_model(cfg,folder_name):
 
     return model_instance, start_epoch
 
-def save_model(cfg, epoch, model, stats,folder_name):
+def save_model(cfg, epoch, model, stats):
     # make sure save directory exists; create if not
-    os.makedirs(f'model_states/{folder_name}', exist_ok=True)
+    folder_name = cfg['folder_name']
+    os.makedirs(f'baseline/model_states/{folder_name}', exist_ok=True)
 
     # get model parameters and add to stats...
     stats['model'] = model.state_dict()
 
     # ...and save
-    torch.save(stats, open(f'model_states/{folder_name}{epoch}.pt', 'wb'))
+    torch.save(stats, open(f'baseline/model_states/{folder_name}{epoch}.pt', 'wb'))
     
     # also save config file if not present
-    cfpath = f'model_states/{folder_name}config.yaml'
+    cfpath = f'baseline/model_states/{folder_name}config.yaml'
     if not os.path.exists(cfpath):
         with open(cfpath, 'w') as f:
             yaml.dump(cfg, f)
 
-def train(model, data_loader, loss_fn, optimiser, device):
+def train(model, data_loader, loss_fn, optimiser, metric_fn, device):
     
     num_batches = len(data_loader)
-    metric_fn = MultilabelF1Score(num_labels=42).to(device)
-        
     model.train()
     ## running averages
     loss_total, metric_total = 0.0, 0.0
@@ -112,7 +103,7 @@ def train(model, data_loader, loss_fn, optimiser, device):
     
     return loss_total, metric_total
 
-def validate(model, data_loader, loss_fn, device):
+def validate(model, data_loader, loss_fn, metric_fn, device):
 
     '''
         Validation function. Looks like training
@@ -120,7 +111,7 @@ def validate(model, data_loader, loss_fn, device):
     '''
 
     num_batches = len(data_loader)
-    metric_fn = MultilabelF1Score(num_labels=42).to(device)
+    
     # put the model into evaluation mode
     model.eval()
 
@@ -172,13 +163,13 @@ def main():
     # init random number generator seed (set at the start)
     init_seed(cfg.get('seed', None))
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device {device}")
 
     # Define Transformation
 
     resamp = torchaudio.transforms.Resample(
-            orig_freq=SAMPLE_RATE,#22050,
+            orig_freq=22050,
             new_freq=16000
             )
     mel_spectrogram = torchaudio.transforms.MelSpectrogram(
@@ -214,118 +205,102 @@ def main():
         'metadata.csv'
     )
     
-    #ANNOTATIONS_FILE = 'metadata_sample1000.csv'
-
     AUDIO_DIR = os.path.join(
         cfg['data_root'],
         'audio'
     )
-    
-    BATCH_SIZE = cfg['batch_size']
-    EPOCHS = cfg['num_epochs']
-    LEARNING_RATE = cfg['learning_rate']
-    
-    import pandas as pd
-    
-    df = pd.read_csv(ANNOTATIONS_FILE)
-
-    for site in ['INCT4','INCT20955']:
         
-        folder_name = cfg['model_type'] + '_without_' + site +'/'
-        df_site = df[df['site']!=site]   
-        training_data = AnuraSet(
-            annotations_file=df_site, 
+    training_data = AnuraSet(
+            annotations_file=ANNOTATIONS_FILE, 
             audio_dir=AUDIO_DIR, 
             transformation=train_transform,
             train=True
+                        )
+    print(f"There are {len(training_data)} samples in the training set.")
+
+    # TODO: call val not test!
+    val_data = AnuraSet(
+            annotations_file=ANNOTATIONS_FILE, 
+            audio_dir=AUDIO_DIR, 
+            transformation=val_transform,
+            train=False
                             )
-        print('Training model without site', site)
-        print(f"There are {len(training_data)} samples in the training set.")
+    print(f"There are {len(val_data)} samples in the test set.")
 
-        # TODO: call val not test!
-        val_data = AnuraSet(
-                annotations_file=df_site, 
-                audio_dir=AUDIO_DIR, 
-                transformation=val_transform,
-                train=False
+    train_dataloader = DataLoader(training_data, 
+                                batch_size=cfg['batch_size'],
+                                shuffle=True,
+                                drop_last=True,
+                                pin_memory=True,
+                                num_workers=4,
                                 )
-        print(f"There are {len(val_data)} samples in the test set.")
+                                
+    val_dataloader = DataLoader(val_data, 
+                                batch_size=cfg['batch_size'],
+                                shuffle=True,
+                                drop_last=True,
+                                pin_memory=True,
+                                num_workers=4,
+                                )
 
-        train_dataloader = DataLoader(training_data, 
-                                    batch_size=BATCH_SIZE,
-                                    shuffle=True,
-                                    drop_last=True,
-                                    pin_memory=True,
-                                    num_workers=4,
-                                    )
-                                    
-        val_dataloader = DataLoader(val_data, 
-                                    batch_size=BATCH_SIZE,
-                                    shuffle=True,
-                                    drop_last=True,
-                                    pin_memory=True,
-                                    num_workers=4,
-                                    )
+    multi_label=cfg['multilabel']   
+    if multi_label:
+        loss_fn = nn.BCEWithLogitsLoss()
+    else:
+        loss_fn = nn.CrossEntropyLoss()
+    
+    # initialize model
+    model_instance, current_epoch = load_model(cfg)
+    
+    model_instance.to(device)
+    
+    optimiser = torch.optim.Adam(model_instance.parameters(),
+                                lr=cfg['learning_rate'])
+    
+    metric_fn = MultilabelF1Score(num_labels=cfg['num_classes']).to(device)
+    
+    start = time()
+    progress_bar_epoch = trange(cfg['num_epochs'])
+    print('Starting training')
+    for current_epoch in range(current_epoch, cfg['num_epochs']):
+        start_epoch = time()
+        current_epoch += 1
 
-        
-         # initialise loss funtion + optimiser
-        multi_label=cfg['multilabel']   
-        if multi_label:
-            loss_fn = nn.BCEWithLogitsLoss()
-        else:
-            loss_fn = nn.CrossEntropyLoss()
-            
-        #model_instance = ResNetClassifier(model_type=cfg['model_type'],
-        #                        ).to(device)
-       # train model
-        #current_epoch = 0
-        
-        # initialize model
-        model_instance, current_epoch = load_model(cfg,folder_name)
-        
-        model_instance.to(device)
-        
-        optimiser = torch.optim.Adam(model_instance.parameters(),
-                                    lr=LEARNING_RATE)
-        
-        start = time()
-        progress_bar_epoch = trange(EPOCHS)
-        print('Starting training')
-        for current_epoch in range(current_epoch, EPOCHS):
-            start_epoch = time()
-            current_epoch += 1
-            
-            loss_train, metric_train = train(model_instance, train_dataloader, loss_fn, optimiser, device)
-            loss_val, metric_val = validate(model_instance, val_dataloader, loss_fn, device)
-            progress_bar_epoch.update(1)
-            progress_bar_epoch.write(
-            'Epoch: {:.0f}: Loss val: {:.4f} ; F1-score macro val: {:.4f} - Epoch time: {:.1f}s; Total time: {:.1f}s - {:.0f}%'.format(
-                (current_epoch),
-                loss_val,
-                metric_val,
-                (time()-start_epoch),
-                (time()-start),
-                (100*(current_epoch)/EPOCHS)
-            )
-            )
+        loss_train, metric_train = train(model_instance, train_dataloader, 
+                                         loss_fn, optimiser, metric_fn, device)
+        loss_val, metric_val = validate(model_instance, val_dataloader, 
+                                        loss_fn, metric_fn, device)
+        progress_bar_epoch.update(1)
+        progress_bar_epoch.write(
+        'Epoch: {:.0f}: Loss val: {:.4f} ; F1-score macro val: {:.4f} - Epoch time: {:.1f}s; Total time: {:.1f}s - {:.0f}%'.format(
+            (current_epoch),
+            loss_val,
+            metric_val,
+            (time()-start_epoch),
+            (time()-start),
+            (100*(current_epoch)/cfg['num_epochs'])
+        )
+        )
 
-            # combine stats and save
-            stats = {
-                'loss_train': loss_train,
-                'loss_val': loss_val,
-                'metric_train': metric_train,
-                'metric_val': metric_val
-            }
-            # TODO: wandb or YAML?
-            save_model(cfg, current_epoch, model_instance, stats,folder_name)
-        progress_bar_epoch.close()
-        print("Finished training")
+        # combine stats and save
+        stats = {
+            'loss_train': loss_train,
+            'loss_val': loss_val,
+            'metric_train': metric_train,
+            'metric_val': metric_val
+        }
+        # TODO: wandb or YAML?
+        save_model(cfg, current_epoch, model_instance, stats)
+    progress_bar_epoch.close()
+    print("Finished training")
 
-        # save model
-        torch.save(model_instance.state_dict(), folder_name+'final.pth')
-        print("Trained feed forward net saved at "+folder_name+'final.pth')
+    # save model
+    torch.save(model_instance.state_dict(), 
+               'baseline/model_states/'+cfg['folder_name']+'final.pth')
+               
+    print('sTrained feed forward net saved at ' +
+        'baseline/model_states/'+cfg['folder_name']+'final.pth')
 
 if __name__ == '__main__':
     # This block only gets executed if you call the "train.py" script directly
-    # (i.e., "python ct_classifier/train.py").
     main()
